0765ab7bdecb5563be57d1dc269cae1a48348396
Alpacorp <alejandro.palacios88@gmail.com>
2023-01-11 21:12:11 -0500

    add new config lib

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,23 +1,27 @@
-// rollup.config.js
 import typescript from "@rollup/plugin-typescript";
 import commonjs from "@rollup/plugin-commonjs";
 
 export default {
-  input: "src/index.ts",
+  input: "src/main.tsx",
   output: {
     dir: "output",
     format: "cjs",
   },
   plugins: [
-    typescript(),
+    typescript({
+      tsconfig: "./tsconfig.json",
+      compilerOptions: {
+        module: "CommonJS",
+        target: "ES2016",
+      },
+    }),
     commonjs({
-      extensions: [".js", ".ts"],
+      extensions: [".js", ".ts", ".tsx"],
       dynamicRequireTargets: [
         "node_modules/@hubspot/api-client/**",
         "node_modules/@hubspot/api-client/lib/**",
-        "node_modules/@hubspot/api-client/lib/crm/**",
+        "node_modules/@hubspot/api-client/lib/src/discovery/crm/CrmDiscovery.js",
         "node_modules/@hubspot/api-client/lib/crm/contacts/**",
-        "node_modules/@hubspot/api-client/lib/crm/contacts/basicApi.js",
         "node_modules/@hubspot/api-client/lib/crm/contacts/basicApi.js",
       ],
       ignoreDynamicRequires: true,
