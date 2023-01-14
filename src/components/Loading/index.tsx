6f964ba76692f2a7051660f41c76110ce47e2acf
Alpacorp <alejandro.palacios88@gmail.com>
2023-01-13 20:02:08 -0500

    create Loading component

---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,25 @@
+import { FC } from "react";
+import "./styles.css";
+
+interface LoadingProps {
+  open: boolean;
+}
+
+export const Loading: FC<LoadingProps> = ({ open }) => {
+  return (
+    <>
+      {open && (
+        <div className="loading">
+          <div className="loading__spinner" />
+          <div className="wave">👋</div>
+          <div>
+            <h2>
+              Estamos validando la información para indicarte la mejor solución
+              a la <strong className="highlight-yellow">Inverfobia</strong>
+            </h2>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
