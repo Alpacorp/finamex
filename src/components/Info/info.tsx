e6f407b99604542bfe0de4816a042c27aaa52b20
Alpacorp <alejandro.palacios88@gmail.com>
2023-01-27 15:33:35 -0500

    change text Info

--- src/components/Info/info.tsx
+++ src/components/Info/info.tsx
@@ -10,7 +10,9 @@ export const Info: FC = () => {
         <div>
           <div className="content">
             <p>
-              <strong>Es el miedo a invertir que paraliza tu dinero </strong>
+              <strong>
+                La Inverfobia es el miedo a invertir que paraliza tu dinero{" "}
+              </strong>
               sin importar estado financiero, género, ni edad.
             </p>
             <p>
