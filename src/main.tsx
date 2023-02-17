85b732fd59622c69d62547c5e003562f0d6ae445
Alpacorp <alejandro.palacios88@gmail.com>
2023-02-17 00:42:28 -0500

    add vercel analytics package

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "@rollbar/react";
+import { rollbarConfig } from "./Telemetry/rollbar-config";
 import { Analytics } from "@vercel/analytics/react";
 
 import { AppRouter } from "./containers/AppRouter";
 
 import { ScoreProvider } from "./context/ScoreContext";
-
-import { rollbarConfig } from "./Telemetry/rollbar-config";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
